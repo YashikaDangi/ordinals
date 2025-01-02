b5c6fe518b41416d183d514afb3d86de3eb0afed
YashikaDangi <yashikadangi10@gmail.com>
2025-01-02 17:29:31 +0530

    first commit

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,66 +1,48 @@
-import vine, { errors } from "@vinejs/vine";
-import { NextRequest, NextResponse } from "next/server";
 import connect from "@/database/mongo.config";
-import ErrorReporter from "@/validator/ErrorReporter";
-import { loginSchema } from "@/validator/authValidationSchema";
 import { User } from "@/models/User";
 import bcrypt from "bcryptjs";
-
-// * Connect to the DB
-connect();
+import vine, { errors } from "@vinejs/vine";
+import { NextRequest, NextResponse } from "next/server";
+import { loginSchema } from "@/validator/authValidationSchema";
+import ErrorReporter from "@/validator/ErrorReporter";
 
 export async function POST(request: NextRequest) {
   try {
-    console.log("Request Object:", request);
-    const body = await request.json();
-    console.log("Request Body:", body);
+    // Ensure database connection
+    await connect();
 
+    const body = await request.json();
     vine.errorReporter = () => new ErrorReporter();
     const validator = vine.compile(loginSchema);
     const output = await validator.validate(body);
 
-    console.log("Validation Output:", output);
-
     const user = await User.findOne({ email: output.email });
-    console.log("User Found:", user);
-
     if (user) {
       const checkPassword = bcrypt.compareSync(output.password!, user.password);
-      console.info("Password Match:", checkPassword);
-
       if (checkPassword) {
         return NextResponse.json(
           { status: 200, message: "User Logged in successfully!" },
           { status: 200 }
         );
       }
-
       return NextResponse.json(
-        {
-          status: 400,
-          errors: { email: "Please check your credentials." },
-        },
-        { status: 200 }
-      );
-    } else {
-      return NextResponse.json(
-        {
-          status: 400,
-          errors: { email: "No User found in our system with above email." },
-        },
+        { status: 400, errors: { email: "Please check your credentials." } },
         { status: 200 }
       );
     }
+
+    return NextResponse.json(
+      { status: 400, errors: { email: "No User found in our system with above email." } },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error Occurred:", error);
-
     if (error instanceof errors.E_VALIDATION_ERROR) {
       return NextResponse.json(
         { status: 400, errors: error.messages },
         { status: 200 }
       );
     }
-
     return NextResponse.json(
       { status: 500, errors: { message: "Internal Server Error" } },
       { status: 500 }
@@ -68,6 +50,4 @@ export async function POST(request: NextRequest) {
   }
 }
 
-
 export const dynamic = 'force-dynamic';
-
